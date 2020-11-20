5a5d672534c8cc75321420c8abc054ea1cee1c2e
dorgonman <dorgonman@hotmail.com>
2020-11-20 23:48:14 +0800

    Add Mannequin Character

---
+++ Source/HorizonInteractDemo/Public/MyFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORIZONINTERACTDEMO_API UMyFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
