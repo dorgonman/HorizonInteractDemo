5143a8fa562c06b4206cb7191e4e592c0e4c8100
dorgon.chang <dorgonman@hotmail.com>
2022-11-16 23:53:50 +0800

    Fix 5.1.0 compile warning and error

---
+++ Build/Script/HorizonInteractDemo.Automation/HorizonInteractDemoTest.cs
@@ -0,0 +1,75 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gauntlet;
+using System.IO;
+using System.IO.Compression;
+
+namespace UnrealGame
+{
+
+	public class HorizonInteractDemoTestConfig : UnrealTestConfiguration
+	{
+		/// <summary>
+		/// Map to use
+		/// </summary>
+
+		
+		[AutoParam]
+		public string AdditionalCommandLine = "";
+		
+		[AutoParam]
+		public string ExecCmds = "automation SetMinimumPriority Critical;RunTests Filter:Plugin+Filter:_Game+Filter:Project.Functional Tests;Quit";
+        
+
+        /// <summary>
+        /// Applies these options to the provided app config
+        /// </summary>
+        /// <param name="AppConfig"></param>
+        public override void ApplyToConfig(UnrealAppConfig AppConfig, UnrealSessionRole ConfigRole, IEnumerable<UnrealSessionRole> OtherRoles)
+		{
+			base.ApplyToConfig(AppConfig, ConfigRole, OtherRoles);
+
+			AppConfig.CommandLine += " -ExecCmds=\"" + ExecCmds + "\" -FORCELOGFLUSH";
+			AppConfig.CommandLine += AdditionalCommandLine;
+			
+	
+		}
+	}
+	public class HorizonInteractDemoTest : UnrealTestNode<HorizonInteractDemoTestConfig>
+	{
+		public HorizonInteractDemoTest(UnrealTestContext InContext) : base(InContext)
+		{
+		}
+
+		public override HorizonInteractDemoTestConfig GetConfiguration()
+		{
+			// just need a single client
+			HorizonInteractDemoTestConfig Config = base.GetConfiguration();
+			Config.MaxDuration = 5 * 600;		// 5min should be plenty
+			int ClientCount = Context.TestParams.ParseValue("numclients", 1);
+			bool WithServer = Context.TestParams.ParseParam("server");
+
+			if (ClientCount > 0)
+			{
+				Config.RequireRoles(UnrealTargetRole.Client, ClientCount);
+			}
+
+			if (WithServer)
+			{
+				Config.RequireRoles(UnrealTargetRole.Server, 1);
+			}
+
+			UnrealTestRole ClientRole = Config.RequireRole(UnrealTargetRole.Client);
+
+			//ClientRole.Controllers.Add("HorizonQuestDemo");
+			return Config;
+		}
+
+	}
+
+}
